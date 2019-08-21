a6ed7d499c5a9abf768544a9f586618eb7cd2b28
Raul Llerena <raul.llerena@dbaccess.com>
2019-08-20 23:56:02 -0500

    flyway

--- FinanzasPersonales/src/main/java/com/rllerena/controller/MtnCategoria.java
+++ FinanzasPersonales/src/main/java/com/rllerena/controller/MtnCategoria.java
@@ -3,7 +3,8 @@ package com.rllerena.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController       (value = "api")
+@RestController
+@RequestMapping(value = "api")
 public class MtnCategoria {
 
     @RequestMapping("/")
